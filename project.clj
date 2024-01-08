6b584c21f5700b955061c4c0323e70257f02f4e3
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-01-08 13:14:31 +0900

    add ring to dependencies

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring "1.11.0"]]
   :repl-options {:init-ns todo-clj.core})
+
