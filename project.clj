21fd9ee3b37b02fa6bf0374db133e01e4ca5be06
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-01-08 16:37:46 +0900

    install compojure

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [ring "1.11.0"]]
+                 [ring "1.11.0"]
+                 [compojure "1.7.0"]]
   :repl-options {:init-ns todo-clj.core})
 
