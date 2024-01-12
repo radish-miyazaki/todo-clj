021c2150635b788db8e07186630c2e04d922feec
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-01-13 04:54:28 +0900

    wip: implementing database file...

---
+++ src/todo_clj/db.clj
@@ -0,0 +1,16 @@
+(ns todo-clj.db
+  (:require
+   [clojure.java.jdbc :as jdbc]))
+
+(def db-spec {:dbtype "postgresql"
+              :dbname "todo_clj_dev"
+              :host "localhost"
+              :port 5432
+              :user "todoclj-user"
+              :password "todoclj-pass"})
+
+(defn migrate []
+  (jdbc/db-do-commands
+   db-spec
+   (jdbc/create-table-ddl :todo [:id :serial] [:title :varchar])))
+
