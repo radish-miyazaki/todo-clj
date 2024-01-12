723912fd6fc497560a0a04b1111eb681fa5acabc
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-01-12 11:24:20 +0900

    finished implemented view files

--- src/todo_clj/view/main.clj
+++ src/todo_clj/view/main.clj
@@ -3,8 +3,8 @@
    [hiccup2.core :as hc]))
 
 (defn home-view [_]
-  (-> (list
-       [:h1 "ホーム画面"]
-       [:a {:href "/todo"} "TODO 一覧"])
-      hc/html))
+  (-> (list [:h1 "ホーム画面"]
+            `[:a {:href "/todo"} "TODO 一覧"])
+      hc/html
+      str))
 
--- src/todo_clj/view/todo.clj
+++ src/todo_clj/view/todo.clj
@@ -1,27 +1,12 @@
 (ns todo-clj.view.todo
   (:require
-   [hiccup.core :as hc]))
+   [hiccup2.core :as hc]))
 
-(defn todo-index-view [req todo-list]
+(defn todo-index-view [_ todo-list]
   (-> `([:h1 "TODO 一覧"]
         [:ul
          ~@(for [{:keys [title]} todo-list]
-             [:li title])])
-      hc/html))
-
-;; (ns todo-clj.view.todo
-;;   (:require
-;;    [hiccup2.core :as hc]))
-;;
-;; (defn todo-index-view [_ todo-list]
-;;   (-> `([:h1 "TODO 一覧"]
-;;         [:ul
-;;          ~@(for [{:keys [title]} todo-list]
-;;               [:li title])])
-;;       hc/html))
-;; (defn todo-index-view [req todo-list]
-;;   (hc/html (list [:h1 "TODO 一覧"]
-;;        `[:ul
-;;           ~@(for [{:keys [title]} todo-list]
-;;               [:li title])])))
+              [:li title])])
+      hc/html
+      str))
 
