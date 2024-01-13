78e539203096e92dad08ff7db9c350535ac1d945
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-01-13 23:09:27 +0900

    refactor dependencies vec

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [org.postgresql/postgresql "42.7.1"]
                  [ring "1.11.0"]
                  [compojure "1.7.0"]
                  [environ "1.2.0"]
-                 [hiccup "2.0.0-RC2"]
-                 [org.clojure/java.jdbc "0.7.12"]
-                 [org.postgresql/postgresql "42.7.1"]]
+                 [hiccup "2.0.0-RC2"]]
   :plugins [[lein-environ "1.2.0"]]
   :repl-options {:init-ns todo-clj.core}
   :profiles {:dev {:dependencies [[prone "2021-04-23"]]
